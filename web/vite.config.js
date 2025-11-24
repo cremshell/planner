a55d4015b01ccb4a320a0ea91a24ee1d73f2be65
cremshell <cremshel@gmail.com>
2025-11-24 22:51:57 +0900

    config: add custom Vite configuration

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,3 +1,19 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({});
+export default defineConfig({
+  appType: 'spa',
+  resolve: {
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  css: {
+    devSourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5001,
+    open: true,
+  },
+  build: {
+    sourcemap: true,
+  },
+});
